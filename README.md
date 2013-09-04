11524a0e9c68eb39ad2d05e4ddd5a03ed8add25a
Tom Boutell <tom@punkave.com>
2013-09-04 13:30:32 -0400

    It works!

---
+++ README.md
@@ -0,0 +1,282 @@
+# Apostrophe Site
+
+The Apostrophe Site module makes it easy to build websites with the [Apostrophe](http://github.com/punkave/apostrophe) content management system for Node.
+
+## Installation
+
+It is easiest to start by cloning the [Apostrophe sandbox project](http://github.com/punkave/apostrophe-sandbox) and pushing it up to your own repository. But skipping that step is pretty easy too. Let's assume you're starting from scratch.
+
+Apostrophe itself requires:
+
+* [node](http://nodejs.org/), of course. You must have at least version `0.10`
+* [mongodb](http://www.mongodb.org/) **version 2.2 or better**, on your local machine (or point to another database server)
+* imagemagick, to resize uploaded images (specifically the `convert` and `identify` command line tools)
+
+Create a new git project, then run `npm install apostrophe-site` to install the module.
+
+## Configuring Your Site
+
+Here's an `app.js` that demonstrates most of the options. Most of this is optional, of course. `root`, `shortName`, `hostName`, `adminPassword` and `sessionSecret` are required.
+
+    var site = require('apostrophe-site')({
+      // Allows apostrophe-sites to require stuff
+      // on our behalf and also find our root folder
+      root: module,
+
+      // Used to name the local mongodb database,
+      // if you don't pass a db option with more details
+      shortName: 'mysite',
+
+      // Hostname you plan to give your site
+      hostName: 'mysite.com',
+
+      // Apostrophe sizes your images to several awesome sizes right out of the box,
+      // but we're greedy and we want something bigger than full (1280)
+      addImageSizes: [
+        {
+          name: 'max',
+          width: 1600,
+          height: 1280
+        }
+      ],
+
+      // You can always log in at /login as admin, with this password
+      adminPassword: 'SOMETHING SECURE PLEASE',
+
+      redirectAfterLogin: function(user) {
+        if (user.permissions.admin) {
+          return '/awesomepeople';
+        } else {
+          return '/coolpeople';
+        }
+      },
+
+      sessionSecret: 'SOMETHING RANDOM PLEASE',
+
+      // Any options accepted by the apostrophe-pages module,
+      // such as tabOptions and descendantOptions
+      pages: {
+        // List all the page types users should be able to add here, including
+        // things like "Blog" and "Events" that are powered by modules, so you get
+        // to pick the order
+        types: [
+          // TODO double check this doesn't get ignored if blog is added later and wasn't wanted
+          { name: 'default', label: 'Default (Two Column)' },
+          { name: 'home', label: 'Home Page' },
+          { name: 'blog', label: 'Blog' },
+          { name: 'events', label: 'Events' }
+        ]
+
+        // Load descendants of homepage and current page two levels deep
+        // instead of one
+        tabOptions: { depth: 2 },
+        descendantOptions: { depth: 2 },
+
+        // Do something special if the URL doesn't match anything else
+        notfound: function(req, callback) {
+          if (req.url === '/special') {
+            req.redirect = '/specialer';
+          }
+          return callback(null);
+        }
+
+        // Run some middleware on the route that serves pages
+        middleware: [ /* middleware, functions, galore */ ],
+
+        // Custom page loader functions beyond those automatically
+        // provided. Already you have the page with the slug 'global'
+        // available at all times, the current page, its tabs, its
+        // descendants, and anything loaded on behalf of your modules,
+        // like blog posts appearing on the current page
+        load: [
+          function(req, callback) {
+            if (!(req.page && (req.page.type === 'fancy'))) {
+              // Doesn't concern us
+              return callback(null);
+            }
+            // Set some custom data to be provided to the nunjucks template.
+            // We have a callback here, so we could go get anything
+            req.page.extras.fanciness = true;
+            return callback(null);
+          }
+        ],
+      },
+
+      // Let's add the blog and events modules. You must npm install them.
+      // apostrophe-site will require them for you and pass your options
+      modules: {
+        'apostrophe-events': {
+          widget: true
+        },
+        'apostrophe-blog': {
+          widget: true
+        }
+      },
+
+      // Custom command line tasks. Run like this:
+      // node app project:frobulate
+      // argv is powered by optimist
+      tasks: {
+        project: {
+          frobulate: function(apos, argv, callback) {
+            console.log('Frobulated the hibblesnotz');
+            console.log('You passed these arguments: ' + argv._);
+            return callback(null);
+          }
+        }
+      },
+
+      locals: {
+        // Extra locals visible to every nunjucks template. Functions and
+        // data are both fair game
+        embiggen: function(s) {
+          return s * 1000;
+        }
+      },
+
+      assets: {
+        // Loads site.js from public/js
+        scripts: [
+          'site'
+        ],
+        // Loads site.less from public/css
+        stylesheets: [
+          'site'
+        ]
+      },
+
+      // Last best chance to set custom Express routes
+      setRoutes: function(callback) {
+        site.app.get('/wacky', function(req, res) { res.send('wackiness'); });
+        return callback(null);
+      },
+
+      // Just before listen. Last chance to set up anything
+      afterInit: function(callback) {
+        return callback(null);
+      },
+    });
+
+## Overriding the Templates of a Module
+
+First `npm install` and configure `apostrophe-blog`. Then create a `lib/modules/apostrophe-blog/views` folder in your project. Copy any templates you wish to customize from the npm module's views folder to `lib/modules/apostrophe-blog/views`.
+
+Boom! Apostrophe will automatically look first at your "project level" module folder.
+
+## Overriding a Module With a New Name
+
+You can override a module more than once, for instance to set up two things that are similar in spirit to a blog. Just create folders in `lib/modules`, with your `views` overrides, and configure them in `app.js` via the `modules` option as shown above. Then use the `extend` property to tell Apostrophe what module you're extending.
+
+You'll want to set the `name` and `instance` options so the database can distinguish between your stories and regular blog posts:
+
+    stories: {
+      extend: 'apostrophe-blog',
+      name: 'stories',
+      instance: 'story',
+      addFields: [
+        {
+          name: 'storyteller',
+          type: 'string'
+        }
+      ]
+    }
+
+Note that you will need to copy the `new`, `edit` and `manage` templates to your `views` folder and fix any references to `blog` and `blog-post` to refer to `stories` and `story`.
+
+## Overriding the Schema of a Module: Adding Custom Properties
+
+As seen above, you can add and alter the properties of blog posts and similar things via the `addFields` and `alterFields` options as described in the [apostrophe-snippets](http://github.com/punkave/apostrophe-snippets) documentation. Those options can go right in the configuration for your module in `app.js`.
+
+## Overriding and Extending Methods of a Module
+
+If you really need to change a module's behavior, for instance changing what the page loader function does or the way it fetches data from the database, you'll need to subclass it. But we've made subclassing much easier. Just create an `index.js` file in your `lib/modules/mymodulename` folder.
+
+Here's a really simple subclass that changes the way the `index` method of the blog behaves so that a featured story is available to the `index.html` template as the `featured` variable in nunjucks:
+
+    module.exports = stories;
+
+    function stories(options, callback) {
+      return new stories.Stories(options, callback);
+    }
+
+    stories.Stories = function(options, callback) {
+      var self = this;
+
+      module.exports.Super.call(this, options, null);
+
+      self.superIndex = self.index;
+      self.index = function(req, snippets, callback) {
+        self.get(req, { tags: 'featured' }, { limit: 1 }, function(err, results) {
+          if(err) {
+            callback(err);
+          }
+          if(results.total > 0) {
+            req.extras.featured = results.snippets[0];
+          }
+          self.superIndex(req, snippets, callback);
+        });
+      };
+
+      if (callback) {
+        process.nextTick(function() { return callback(null); });
+      }
+
+    };
+
+Note the use of `module.exports.Super`. This automatically points to the base class constructor.
+
+Confused? Just remember to follow this pattern and put your method overrides after the call to `module.exports.Super`.
+
+## Tip: Subclassing Snippets is Often a Good Idea
+
+If it doesn't smell like a blog post, you probably want to subclass snippets instead. The blog module simply subclasses snippets and adds the idea of a publication date.
+
+## Modules Can Have Nothing To Do With Snippets
+
+You can configure modules that have nothing at all to do with snippets, too. Our own RSS and Twitter modules, for instance.
+
+To configure a module with `apostrophe-site`, all you have to do is make sure it looks like this:
+
+    module.exports = factory;
+
+    function factory(options, callback) {
+      return new Construct(options, callback);
+    }
+
+    function Construct(options, callback) {
+      var self = this;
+      // Add a bunch of methods to self here
+      return callback(null);
+    }
+
+    // Export the constructor so others can subclass
+    factory.Construct = Construct;
+
+In a nutshell: you must export a factory function, and it must have a constructor as its `Construct` property.
+
+You can write custom modules in `lib/modules`, or install them with npm. If you use `lib/modules`, your module's code must load from `lib/modules/mymodulename/index.js`.
+
+## More Modules, More Documentation
+
+See [apostrophe](http://github.com/punkave/apostrophe),
+[apostrophe-sandbox](http://github.com/punkave/apostrophe-sandbox),
+[apostrophe-pages](http://github.com/punkave/apostrophe-pages),
+[apostrophe-snippets](http://github.com/punkave/apostrophe-snippets),
+[apostrophe-blog](http://github.com/punkave/apostrophe-blog),
+[apostrophe-events](http://github.com/punkave/apostrophe-events),
+[apostrophe-map](http://github.com/punkave/apostrophe-map),
+[apostrophe-groups](http://github.com/punkave/apostrophe-groups),
+[apostrophe-people](http://github.com/punkave/apostrophe-people),
+[apostrophe-rss](http://github.com/punkave/apostrophe-rss) and
+[apostrophe-twitter](http://github.com/punkave/apostrophe-twitter).
+
+Also browse the [`apostrophe` tag on npm](https://npmjs.org/browse/keyword/apostrophe).
+
+## Community
+
+You should join the [apostrophenow Google Group](https://groups.google.com/forum/?fromgroups#!forum/apostrophenow) for discussion of both Apostrophe 1.5 and Apostrophe 2.
+
+## Thanks for using Apostrophe!
+
+(http://punkave.com)[P'unk Avenue]
+
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+// Currently limited to a single instance due to dependency on Appy.
+// TODO: consider making appy support multiple instances or removing
+// the need for Appy
+
 var appy = require('appy');
 var async = require('async');
 var uploadfs = require('uploadfs');
 var fs = require('fs');
-var apos = require('apostrophe')();
+var path = require('path');
 var _ = require('underscore');
 var extend = require('extend');
-var appy = require('appy');
-var apostrophe = require('apostrophe');
-var pages = require('apostrophe-pages');
 
 module.exports = function(options) {
   return new AposSite(options);
@@ -16,11 +17,20 @@ module.exports = function(options) {
 function AposSite(options) {
   var self = this;
 
-  self.apos = apostrophe();
+  self.apos = require('apostrophe')();
   self.root = options.root;
+  self.rootDir = path.dirname(self.root.filename);
+
+  // Fetch local overrides for this server, like minify: true or uploadfs configuration
+  if (fs.existsSync(self.rootDir + '/data/local.js')) {
+    var local = require(self.rootDir + '/data/local.js');
+    extend(true, options, local);
+  }
+
   self.uploadfs = uploadfs();
+
   if (!self.root) {
-    throw 'Specify the `root` option and set it to __dirname';
+    throw 'Specify the `root` option and set it to `module` (yes, really)';
   }
   self.shortName = options.shortName;
   if (!self.shortName) {
@@ -34,13 +44,12 @@ function AposSite(options) {
   if (!self.adminPassword) {
     throw "Specify the `adminPassword` option and set it to a secure password for admin access. This account is always valid in addition to accounts in the database";
   }
-  self.imageSizes = apos.defaultImageSizes;
+  self.imageSizes = self.apos.defaultImageSizes;
   if (options.imageSizes) {
-    if (options.imageSizes.$append) {
-      self.imageSizes = self.imageSizes.concat(options.imageSizes);
-    } else {
-      self.imageSizes = options.imageSizes;
-    }
+    self.imageSizes = options.imageSizes;
+  }
+  if (options.addImageSizes) {
+    self.imageSizes = self.imageSizes.concat(options.addImageSizes);
   }
   if (!options.sessionSecret) {
     throw "Specify the `sessionSecret` option. This should be a secure password not used for any other purpose.";
@@ -51,12 +60,12 @@ function AposSite(options) {
 
   var uploadfsDefaultSettings = {
     backend: 'local',
-    uploadsPath: self.root + '/public/uploads',
+    uploadsPath: self.rootDir + '/public/uploads',
     uploadsUrl: '/uploads',
-    tempPath: self.root + '/data/temp/uploadfs',
+    tempPath: self.rootDir + '/data/temp/uploadfs',
     // Register Apostrophe's standard image sizes. Notice you could
     // concatenate your own list of sizes if you had a need to
-    imageSizes: apos.defaultImageSizes.concat([])
+    imageSizes: self.imageSizes
   };
   uploadfsSettings = {};
   extend(true, uploadfsSettings, uploadfsDefaultSettings);
@@ -67,11 +76,11 @@ function AposSite(options) {
     // render anything directly
     auth: self.apos.appyAuth({
       loginPage: function(data) {
-        return self.modules.pages.decoratePageContent({ content: apos.partial('login', data), when: 'anon' });
+        return self.pages.decoratePageContent({ content: self.apos.partial('login', data), when: 'anon' });
       },
       redirect: function(user) {
         if (options.redirectAfterLogin) {
-          return options.redirectAfterLogin();
+          return options.redirectAfterLogin(user);
         }
         return '/';
       },
@@ -85,18 +94,19 @@ function AposSite(options) {
     db: {
       host: (options.db && options.db.host) || 'localhost',
       port: (options.db && options.db.port) || 27017,
+      name: (options.db && options.db.name) || options.shortName,
       collections: (options.db && options.db.collections) || []
     },
 
     // Supplies LESS middleware
-    static: self.root + '/public',
+    static: self.rootDir + '/public',
 
     ready: function(appArg, dbArg)
     {
       self.app = appArg;
       self.db = dbArg;
 
-      async.series([ createTemp, initUploadfs, initApos, initModules, bridgeModules, setRoutes ], listen);
+      async.series([ createTemp, initUploadfs, initApos, initPages, initModules, bridgeModules, setRoutes, servePages, pushAssets, endAssets, afterInit ], go);
     }
   });
 
@@ -127,7 +137,7 @@ function AposSite(options) {
       uploadfs: self.uploadfs,
       locals: self.locals,
       // Allows us to extend shared layouts
-      partialPaths: [ self.root + '/views/global' ],
+      partialPaths: [ self.rootDir + '/views/global' ],
       minify: self.minify
     }, callback);
   }
@@ -135,7 +145,7 @@ function AposSite(options) {
   function initPages(callback) {
     var pagesOptions = {};
     extend(true, pagesOptions, options.pages);
-    pagesOptions.app = self.apos;
+    pagesOptions.apos = self.apos;
     pagesOptions.app = self.app;
     self.pages = require('apostrophe-pages')(pagesOptions, callback);
   }
@@ -148,17 +158,128 @@ function AposSite(options) {
     }
     return async.eachSeries(_.keys(modulesConfig), function(name, callback) {
       var config = modulesConfig[name];
-      var local = root + '/lib/modules/' + name + '/index.js';
+      _.defaults(config, {
+        app: self.app,
+        apos: self.apos,
+        pages: self.pages
+      });
+      var localFolder = self.rootDir + '/lib/modules/' + name;
+      var localIndex = localFolder + '/index.js';
+      var npmName = config.extend || name;
+      var localFound = false;
+      var npmFound = false;
+      // Factory function accepts options and callback, returns an object to manage this module
       var factory;
-      if (fs.existsSync(local)) {
-        factory = require(local);
+      if (!fs.existsSync(localFolder)) {
+        // Directly installing an npm module with no subclassing of any kind,
+        // not even a folder with alternate templates. That's allowed
+        factory = self.root.require(npmName);
+        npmFound = true;
       } else {
-        factory = require(name);
+        localFound = true;
+        // Module exists locally. Was it also installed via npm? If so, subclass
+        var base;
+        try {
+          base = self.root.require(npmName);
+          npmFound = true;
+        } catch (e) {
+          // That's OK, this module simply only exists locally
+        }
+        if (!base) {
+          // Exists locally only (well, it had better)
+          try {
+            factory = require(localIndex);
+          } catch (e) {
+            console.error('Unable to find ' + localIndex + '. Either you forgot to npm install something, or you forgot to set the extend property for this module.');
+            throw e;
+          }
+        } else {
+          // Inject a subclass at this point which provides the
+          // right folder name for templates, so we can skip
+          // index.js locally entirely or have one that doesn't bother
+          // with that tedious step
+          var InlineConstruct = function(optionsArg, callback) {
+            var self = this;
+            // Locate the constructor of the base. This ought to be
+            // base.Construct but we didn't think that far ahead, so
+            // figure it out if necessary
+            var Super = base.Construct;
+            if (!Super) {
+              var npmConstructor = guessConstructor(npmName);
+              Super = base[npmConstructor];
+              if (!Super) {
+                throw "Unable to figure out constructor function name for the module " + npmName + ", my best guess was " + npmConstructor + ". This module must export a function that returns an object when given options and a callback, and the constructor for use when subclassing should be the Construct property of that function, or a property named " + npmConstructor + ". If you get this error for an Apostrophe module please report it as a bug.";
+              }
+            }
+            var options = {};
+            extend(true, options, optionsArg);
+            // We use guessConstructor to come up with a reasonable URL for assets
+            // served by this local module, prefixing it with 'my'. It'll also get passed
+            // through apos.cssName. So "apostrophe-blog" becomes /my-blog.
+            var myConstructor = guessConstructor(name);
+            options.modules = (options.modules || []).concat([{ dir: localFolder, name: 'my' + myConstructor }]);
+            return Super.call(self, options, callback);
+          };
+          var inlineFactory = function(options, callback) {
+            return new InlineConstruct(options, callback);
+          };
+          inlineFactory.Construct = InlineConstruct;
+          // If there is a local subclass, require it and inject our inline class
+          // as its superclass
+          if (fs.existsSync(localIndex)) {
+            factory = require(localIndex);
+            // Make our inline subclass available to the module. It's OK if the
+            // module doesn't care and does its own requiring and subclassing,
+            // but this sure is convenient
+            factory.Super = InlineConstruct;
+          } else {
+            // No local index.js, just template overrides etc. Use the
+            // inline class directly
+            factory = inlineFactory;
+          }
+        }
+      }
+
+      // What should the constructor on the browser side be called?
+      // And what should it extend?
+      //
+      // apostrophe-snippets is smart enough to synthesize it if needed
+
+      if (npmFound && localFound) {
+        if (!config.browser) {
+          config.browser = {};
+        }
+        if (!config.browser.construct) {
+          if (name === npmName) {
+            // Emphasizes we're subclassing something with an otherwise similar name
+            config.browser.construct = 'My' + guessConstructor(name);
+          } else {
+            config.browser.construct = guessConstructor(name);
+          }
+        }
+        if (!config.browser.baseConstruct) {
+          // Browser-side constructors from npm always start with Apos
+          config.browser.baseConstruct = 'Apos' + guessConstructor(npmName);
+        }
+      }
+
+      if (factory.length === 1) {
+        // Requires no callback. Replace it with a wrapper that does
+        var originalFactory = factory;
+        factory = function(config, callback) {
+          setImmediate(function() {
+            return callback(null);
+          });
+          return originalFactory(config);
+        };
       }
       self.modules[name] = factory(config, function(err) {
         if (err) {
           console.error("Error configuring module " + name);
           throw err;
+        }
+        if (!self.modules[name]) {
+          throw 'No module found for ' + name;
         }
         return callback(null);
       });
@@ -169,10 +290,11 @@ function AposSite(options) {
   // This allows the groups module to access the people module, for instance.
   function bridgeModules(callback) {
     _.each(self.modules, function(module, name) {
-      if (module.setModules) {
-        module.setModules(self.modules);
+      if (module.setBridge) {
+        module.setBridge(self.modules);
       }
     });
+    return callback(null);
   }
 
   // Last chance to set routes before the wildcard route for pages
@@ -185,31 +307,33 @@ function AposSite(options) {
   }
 
   function servePages(callback) {
-    // Always set up the page loaders for any active modules that have them
-    var loaders = [];
+    // Always set up the page loaders for any active modules that have them,
+    // and for a virtual page named "global" which is super handy for footers etc.
 
-    _.each(self.modules, function(module) {
+    var loaders = [ ' global' ];
+
+    _.each(self.modules, function(module, name) {
       if (module.loader) {
-        loaders = loaders.push(module.loader);
+        loaders.push(module.loader);
       }
     });
 
     // Append any configured page loaders
-    if (options.pages && options.pages.loaders) {
-      loaders = loaders.concat(options.loaders);
+    if (options.pages && options.pages.load) {
+      loaders = loaders.concat(options.pages.load);
     }
 
     // Extend sensible defaults with custom settings
     var pagesOptions = {};
     extend(true, pagesOptions, {
-      templatePath: __dirname + '/views/pages'
+      templatePath: self.rootDir + '/views/pages'
     });
     extend(true, pagesOptions, options.pages || {});
 
     // The merged loaders must win
-    pagesOptions.loaders = loaders;
+    pagesOptions.load = loaders;
 
-    var serve = pages.serve(pagesOptions);
+    var serve = self.pages.serve(pagesOptions);
 
     // All this does is call app.get('*', ... some middleware ... , serve) but
     // since the middleware option is an array we need to build a complete
@@ -218,14 +342,51 @@ function AposSite(options) {
     var appGetArguments = [ '*' ];
     appGetArguments = appGetArguments.concat(pagesOptions.middleware || []);
     appGetArguments.push(serve);
-    app.get.apply(app, appGetArguments);
+    self.app.get.apply(self.app, appGetArguments);
 
-    // Before we invoke the page server, check for
-    // old-format URLs and hard redirects.
+    return callback(null);
+  }
 
-    // TODO: this wrapper for hard redirects should become
-    // a middleware function, which pages.serve should support.
-    // Then it can move into an apostrophe-redirects module.
+  function pushAssets(callback) {
+    _.each((options.assets && options.assets.stylesheets) || [], function(name) {
+      pushAsset('stylesheet', name);
+    });
+    _.each((options.assets && options.assets.scripts) || [], function(name) {
+      pushAsset('script', name);
+    });
+    function pushAsset(type, name) {
+      return self.apos.pushAsset(type, name, self.rootDir, '');
+    }
+    return callback();
+  }
 
-  return callback(null);
+  function endAssets(callback) {
+    return self.apos.endAssets(callback);
+  }
+
+  function afterInit(callback) {
+    if (options.afterInit) {
+      return options.afterInit(callback);
+    }
+    return callback(null);
+  }
+
+  function go(err) {
+    if (err) {
+      throw err;
+    }
+    // Command line tasks
+    if (self.apos.startTask(options.tasks || {})) {
+      // Chill and let the task run until it's done, don't try to listen or exit
+      return;
+    }
+    return appy.listen();
+  }
+
+  // Convert a module name to the probable name of its constructor property
+  // (we look for Construct first, as newer modules go that way)
+  function guessConstructor(name) {
+    name = name.replace(/^apostrophe\-/, '');
+    return self.apos.capitalizeFirst(self.apos.camelName(name));
+  }
 }
